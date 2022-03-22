a67e3f170fe761a14712f21e3d5ade87033c2488
IskahnX <davidecroce10@gmail.com>
2022-03-22 16:26:09 +0100

    creating table

---
+++ resources/views/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Comics</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-dark bg-dark">
+            <ul style="color: white; list-style-type: none;" class="d-flex flex-row">
+                <li class="px-3">
+                    <a style="text-decoration: none; color:white;" href="{{ route('main') }}">Home</a>
+                </li>
+                <li class="px-3">Comics</li>
+                <li class="px-3">Other</li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+
+</html>
